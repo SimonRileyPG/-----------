cf0ad3b349a5acb5f6efe173a34dce5dd0ae3c6f
SimonRileyPG <ali.celt@bk.ru>
2023-05-12 16:58:03 +0400

    hello

---
+++ my-app/src/Skills.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <div>Programming,Editing Videos, Creating animations</div>
+  )
+}
+
+export default Skills
---
+++ my-app/src/Study.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Study = () => {
+  return (
+    <div>It Step : Начал в 2017 году , Закончил 2019
+    </div>
+  )
+}
+
+export default Study
