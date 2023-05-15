315420766ef0dd1d1566a8e81879b255fd3195ad
SimonRileyPG <ali.celt@bk.ru>
2023-05-15 16:46:54 +0400

    HomeWork №3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Profile from './Profile'
-
+import { useState } from 'react';
 function App() {
+  const [buttonText, setButtonText] = useState('Click');
+  function fear(){
+    console.log("button clicked")
+    setButtonText("clicked");
+    
+  }
   return (
     <div>
+      <button onClick={fear}>{buttonText}</button>
       <Profile />
     </div>
   )
